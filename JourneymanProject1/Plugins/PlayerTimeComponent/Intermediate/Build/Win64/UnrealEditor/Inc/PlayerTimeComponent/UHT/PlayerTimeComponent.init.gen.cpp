82c8bff054f60d302da5d94c2bb44ba7a2abc4a2
swagtiger99 <peacelom05@gmail.com>
2026-02-23 23:54:37 +0000

    Clockwork platform refined further
    I have added a terminal that allows the player to move the platform when interacted with as well as a reusable target point to that designers can set where they want the platform's end point to be

--- JourneymanProject1/Plugins/PlayerTimeComponent/Intermediate/Build/Win64/UnrealEditor/Inc/PlayerTimeComponent/UHT/PlayerTimeComponent.init.gen.cpp
+++ JourneymanProject1/Plugins/PlayerTimeComponent/Intermediate/Build/Win64/UnrealEditor/Inc/PlayerTimeComponent/UHT/PlayerTimeComponent.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodePlayerTimeComponent_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xFEC33158,
-				0x1A5AB992,
+				0x84698BBC,
+				0x7804365C,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_PlayerTimeComponent.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_PlayerTimeComponent.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_PlayerTimeComponent(Z_Construct_UPackage__Script_PlayerTimeComponent, TEXT("/Script/PlayerTimeComponent"), Z_Registration_Info_UPackage__Script_PlayerTimeComponent, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xFEC33158, 0x1A5AB992));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_PlayerTimeComponent(Z_Construct_UPackage__Script_PlayerTimeComponent, TEXT("/Script/PlayerTimeComponent"), Z_Registration_Info_UPackage__Script_PlayerTimeComponent, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x84698BBC, 0x7804365C));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS

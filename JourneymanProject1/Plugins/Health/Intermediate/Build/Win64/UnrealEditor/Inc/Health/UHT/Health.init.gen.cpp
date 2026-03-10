1d00f77b8fa10436eebda92440e26ad85e71549f
xanderjourneymantemp <xanderjourneymantemp@proton.me>
2026-03-10 12:36:31 +0000

    xa_level_seg3update

--- JourneymanProject1/Plugins/Health/Intermediate/Build/Win64/UnrealEditor/Inc/Health/UHT/Health.init.gen.cpp
+++ JourneymanProject1/Plugins/Health/Intermediate/Build/Win64/UnrealEditor/Inc/Health/UHT/Health.init.gen.cpp
@@ -21,7 +21,7 @@ void EmptyLinkFunctionForGeneratedCodeHealth_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x9B8E0B7C,
+				0x3EE65333,
 				0x6BFE2CAE,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -29,5 +29,5 @@ void EmptyLinkFunctionForGeneratedCodeHealth_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_Health.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Health(Z_Construct_UPackage__Script_Health, TEXT("/Script/Health"), Z_Registration_Info_UPackage__Script_Health, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x9B8E0B7C, 0x6BFE2CAE));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Health(Z_Construct_UPackage__Script_Health, TEXT("/Script/Health"), Z_Registration_Info_UPackage__Script_Health, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x3EE65333, 0x6BFE2CAE));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS

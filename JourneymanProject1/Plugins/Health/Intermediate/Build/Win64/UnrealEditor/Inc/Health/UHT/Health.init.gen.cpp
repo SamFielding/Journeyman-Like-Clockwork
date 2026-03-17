44a58bdcabf91d6a3f4314b893593bdd542e214f
Owen <dinsleyowen@gmail.com>
2026-03-17 15:19:19 +0000

    ggggggg

--- JourneymanProject1/Plugins/Health/Intermediate/Build/Win64/UnrealEditor/Inc/Health/UHT/Health.init.gen.cpp
+++ JourneymanProject1/Plugins/Health/Intermediate/Build/Win64/UnrealEditor/Inc/Health/UHT/Health.init.gen.cpp
@@ -21,7 +21,7 @@ void EmptyLinkFunctionForGeneratedCodeHealth_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x2CD879D9,
+				0xF2621B37,
 				0x6BFE2CAE,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -29,5 +29,5 @@ void EmptyLinkFunctionForGeneratedCodeHealth_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_Health.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Health(Z_Construct_UPackage__Script_Health, TEXT("/Script/Health"), Z_Registration_Info_UPackage__Script_Health, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x2CD879D9, 0x6BFE2CAE));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Health(Z_Construct_UPackage__Script_Health, TEXT("/Script/Health"), Z_Registration_Info_UPackage__Script_Health, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xF2621B37, 0x6BFE2CAE));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS

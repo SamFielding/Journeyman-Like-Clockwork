3dec214250b28b7e1c5e2c8093031a87b2f02b0c
Wisdom Lawrence <juniorlawrence2206@gmail.com>
2026-04-15 19:19:53 +0100

    sfddsafsafwa
    sf

--- JourneymanProject1/Plugins/Health/Intermediate/Build/Win64/UnrealEditor/Inc/Health/UHT/Health.init.gen.cpp
+++ JourneymanProject1/Plugins/Health/Intermediate/Build/Win64/UnrealEditor/Inc/Health/UHT/Health.init.gen.cpp
@@ -21,7 +21,7 @@ void EmptyLinkFunctionForGeneratedCodeHealth_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0xDDB526CC,
+				0x57AC7D00,
 				0x6BFE2CAE,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -29,5 +29,5 @@ void EmptyLinkFunctionForGeneratedCodeHealth_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_Health.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Health(Z_Construct_UPackage__Script_Health, TEXT("/Script/Health"), Z_Registration_Info_UPackage__Script_Health, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xDDB526CC, 0x6BFE2CAE));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Health(Z_Construct_UPackage__Script_Health, TEXT("/Script/Health"), Z_Registration_Info_UPackage__Script_Health, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x57AC7D00, 0x6BFE2CAE));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS

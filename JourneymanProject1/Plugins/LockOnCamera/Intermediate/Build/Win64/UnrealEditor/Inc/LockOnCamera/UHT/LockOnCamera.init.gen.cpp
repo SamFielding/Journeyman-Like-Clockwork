7d87e475735a62e4394e735ac5b805b68eb0469c
Alyxandrite005 <joe.cunningham005@gmail.com>
2026-04-23 22:02:26 +0100

    Entire level overhall
    Ive made changes across the whole tower

--- JourneymanProject1/Plugins/LockOnCamera/Intermediate/Build/Win64/UnrealEditor/Inc/LockOnCamera/UHT/LockOnCamera.init.gen.cpp
+++ JourneymanProject1/Plugins/LockOnCamera/Intermediate/Build/Win64/UnrealEditor/Inc/LockOnCamera/UHT/LockOnCamera.init.gen.cpp
@@ -17,7 +17,7 @@ void EmptyLinkFunctionForGeneratedCodeLockOnCamera_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x7DFF5D3D,
+				0xAA54F701,
 				0x69A3288E,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -25,5 +25,5 @@ void EmptyLinkFunctionForGeneratedCodeLockOnCamera_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_LockOnCamera.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_LockOnCamera(Z_Construct_UPackage__Script_LockOnCamera, TEXT("/Script/LockOnCamera"), Z_Registration_Info_UPackage__Script_LockOnCamera, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x7DFF5D3D, 0x69A3288E));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_LockOnCamera(Z_Construct_UPackage__Script_LockOnCamera, TEXT("/Script/LockOnCamera"), Z_Registration_Info_UPackage__Script_LockOnCamera, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xAA54F701, 0x69A3288E));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS

ddc9b1dfad2aedca02b2b1a6fdbf145127f398aa
KatEmily <kathrynemily.hawes@gmail.com>
2026-04-24 10:18:25 +0100

    ertgdvf

--- JourneymanProject1/Plugins/PlayerTimeComponent/Intermediate/Build/Win64/UnrealEditor/Inc/PlayerTimeComponent/UHT/PlayerTimeComponent.init.gen.cpp
+++ JourneymanProject1/Plugins/PlayerTimeComponent/Intermediate/Build/Win64/UnrealEditor/Inc/PlayerTimeComponent/UHT/PlayerTimeComponent.init.gen.cpp
@@ -21,7 +21,7 @@ void EmptyLinkFunctionForGeneratedCodePlayerTimeComponent_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x85D56E24,
+				0x6A5F6922,
 				0xA4C1AE9C,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -29,5 +29,5 @@ void EmptyLinkFunctionForGeneratedCodePlayerTimeComponent_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_PlayerTimeComponent.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_PlayerTimeComponent(Z_Construct_UPackage__Script_PlayerTimeComponent, TEXT("/Script/PlayerTimeComponent"), Z_Registration_Info_UPackage__Script_PlayerTimeComponent, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x85D56E24, 0xA4C1AE9C));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_PlayerTimeComponent(Z_Construct_UPackage__Script_PlayerTimeComponent, TEXT("/Script/PlayerTimeComponent"), Z_Registration_Info_UPackage__Script_PlayerTimeComponent, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x6A5F6922, 0xA4C1AE9C));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
